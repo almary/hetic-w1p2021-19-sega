3d40d157aeae84d8379be633396b9fbc71e3095d
unknown <alexandre.mary.adenni@gmail.com>
2018-12-19 20:07:16 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,12 @@ var pressed = [];
 var submarine;
 var canDrop = true;
 
+// setTimeout(function() {
+//   document.getElementById("play").addEventListener("click", function() {
+    oxo.screens.loadScreen("game", game);
+//   });
+// });
+
 function game() {
   // if (oxo.screens.getCurrentScreen() !== "game") {
   //   return;
@@ -361,13 +367,3 @@ function end() {
     return;
   }
 }
-
-
-
-
-// test à remettre au début
-setTimeout(function() {
-  document.getElementById("play").addEventListener("click", function() {
-    oxo.screens.loadScreen("game", game);
-  });
-});
